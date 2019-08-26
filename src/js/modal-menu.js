429b60a8c684eb88693edf935aa595f85289e848
GusselnikovaA <annanac1993@gmail.com>
2019-08-26 18:03:15 +0300

    кнопка меню при адаптиве.

---
+++ src/js/modal-menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  let menuAlert = $('.menu-alert');
+  let modalMenu = $('#modal-menu');
+  let closeMenu = $('#menu-close');
+
+  menuAlert.on('click', function(){
+    modalMenu.addClass('modal-menu_active');
+  });
+  closeMenu.on('click', function(){
+    modalMenu.removeClass('modal-menu_active');
+  });
+});
